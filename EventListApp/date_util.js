2bf7a1dd59bfe2ffe0d5ef55c6c3ee0992334153
Khoa Nguyen <nkhoa0101@yahoo.com>
2022-02-03 09:37:35 -0800

    fix date display

--- EventListApp/date_util.js
+++ EventListApp/date_util.js
@@ -1,9 +1,9 @@
 export const dateCalc = (value) => {
   let result = new Date();
   result.setTime(value);
-  let year = result.getFullYear();
-  let month = result.getMonth() + 1;
-  let date = result.getDate();
+  let year = result.getUTCFullYear();
+  let month = result.getUTCMonth() + 1;
+  let date = result.getUTCDate();
 
   month = month < 10 ? '0' + month : date;
   date = date < 10 ? '0' + date : date;
